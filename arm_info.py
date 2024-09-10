1e899473040045174ba03c95027ab481c86d8e12
ice-cube-1 <alice.goward@spgs.org>
2024-09-10 21:07:01 +0100

    arm working ish

--- arm_info.py
+++ arm_info.py
@@ -12,8 +12,9 @@ def distance(x: float, y: float) -> float:
     return math.sqrt(x * x + y * y)
 
 class Beam:
-    def __init__(self, length: float):
+    def __init__(self, length: float, absolute = 0.0):
         self.length = length
+        self.absolute = absolute
         self.t: float = 0
         self.endx: float = 0
         self.endy: float = 0
@@ -24,9 +25,9 @@ class Beam:
 
 class Arm:
     def __init__(self, beam0: Beam, beam1: Beam, beam2: Beam, beam3: Beam, clawOpen = 0) -> None:
+        self.beam0 = beam0
         self.beam1 = beam1
         self.beam2 = beam2
-        self.beam0 = beam0
         self.beam3 = beam3
         self.clawOpen = clawOpen
 
@@ -38,7 +39,9 @@ class Arm:
             D1 = math.atan2(y, x)
             D2 = law_of_cosines(dist, self.beam1.length, self.beam2.length)
             self.beam1.t = D1 + D2
+            self.beam1.absolute = self.beam1.t+math.radians(90)
             self.beam2.t = law_of_cosines(self.beam1.length, self.beam2.length, dist)
+            self.beam2.absolute = (self.beam1.absolute+self.beam2.t)-math.radians(180)
             self.beam1.endx = self.beam1.length * math.cos(self.beam1.t)
             self.beam1.endy = self.beam1.length * math.sin(self.beam1.t)+self.beam0.length
             self.beam2.endx = self.beam1.endx - self.beam2.length * math.cos(self.beam1.t + self.beam2.t)
@@ -57,13 +60,11 @@ class Arm:
     def sendToArduino(self) -> None:
         angle = 135-int(math.degrees(self.beam1.t))
         arduino.write(bytes("1" + str(angle) + "\n", 'utf-8'))
-        print(angle)        
         sleep(1)
         angle = 180-int(math.degrees(self.beam2.t)-30)
         arduino.write(bytes("2" + str(angle) + "\n", 'utf-8'))
-        print(angle)
         sleep(1)
-        angle = 270-int(math.degrees(self.beam1.t+self.beam2.t))
+        angle = int(math.degrees(self.beam1.t+self.beam2.t)-90)
         arduino.write(bytes("3" + str(angle) + "\n", 'utf-8'))
         print(angle)
 
@@ -71,11 +72,11 @@ class Arm:
         self.clawOpen = claw_pos.get()
         angle = self.clawOpen*90
         arduino.write(bytes("4" + str(angle) + "\n", 'utf-8'))
-        print(angle)  
 
 def update():
     if arm.setPosition(float(xin.get()), float(yin.get())):
         data = arm.plotInfo()
+        print(math.degrees(arm.beam0.absolute), math.degrees(arm.beam1.absolute), math.degrees(arm.beam2.absolute), math.degrees(arm.beam3.absolute))
         arm.sendToArduino()
         ax.clear()
         ax.plot(data[0], data[1])
@@ -83,11 +84,11 @@ def update():
         ax.set_ylim(0, limit+arm.beam0.length)
         canvas.draw()
 
-arm = Arm(Beam(59.5), Beam(67.6),Beam(67.6),Beam(25))
+arm = Arm(Beam(59.5,math.radians(90)), Beam(67.6),Beam(67.6),Beam(25))
 root = tk.Tk()
 fig, ax = plt.subplots()
 limit = math.sqrt(arm.beam1.length**2 + arm.beam2.length**2+arm.beam0.length**2)
-arduino = serial.Serial(port='COM6',   baudrate=115200)
+arduino = serial.Serial(port='COM6', baudrate=115200)
 
 ax.set_ylim(0, limit+arm.beam0.length)
 ax.set_xlim(-limit, limit)
