a3cc23ae26cf670d5bb33fec567653a6e56ee5d6
ice-cube-1 <alice.goward@spgs.org>
2024-10-08 11:08:13 +0100

    (untested) arduino code updated

--- arduino_code.cpp
+++ arduino_code.cpp
@@ -1,8 +1,9 @@
 #include <Servo.h>
-#include <math.h>
+#include <Stepper.h>
 const int servoCount = 4;
 const int pwmPins[6] = {3,5,6,9,10,11};
 Servo servo[servoCount];
+Stepper stepper(200, 7, 8, 11, 12);
 String data;
 
 void setup() 
@@ -15,6 +16,10 @@ void setup()
 void  loop() {
   while (!Serial.available());
   data = Serial.readString();
-  servo[data.substring(0,1).toInt()-1].write(data.substring(1).toInt());
+  if (data.substring(0,1).toInt() == 5) {
+     stepper.step(data.substring(1).toInt());
+  } else {
+    servo[data.substring(0,1).toInt()-1].write(data.substring(1).toInt());    
+  }
   Serial.println(data.toInt()+1);
 }
