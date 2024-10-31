312130525c282da6fe3dd03a39a6fc0e3b552be4
ice-cube-1 <alice.goward@spgs.org>
2024-10-31 16:02:43 +0000

    removed gitignore js so pi doesn't need tsc

---
+++ static/draw-scene.js
@@ -0,0 +1,203 @@
+function drawScene(gl, programInfo, buffers, cameraRotationX, cameraRotationY, positions, zoom, angle, barrels, stepperpos) {
+    var xpos = -zoom * Math.sin(cameraRotationX) * Math.cos(cameraRotationY);
+    var ypos = zoom * Math.sin(cameraRotationY);
+    var zpos = zoom * Math.cos(cameraRotationX) * Math.cos(cameraRotationY);
+    gl.clearColor(0.8, 0.9, 1.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    const fieldOfView = (45 * Math.PI) / 180;
+    const canvas = gl.canvas;
+    const aspect = canvas.clientWidth / canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 5000.0;
+    const projectionMatrix = mat4.create();
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+    var modelViewMatrix = mat4.create();
+    mat4.lookAt(modelViewMatrix, [xpos, ypos, zpos], [0, 0, 0], [0, 1, 0]);
+    const normalMatrix = mat4.create();
+    mat4.invert(normalMatrix, modelViewMatrix);
+    mat4.transpose(normalMatrix, normalMatrix);
+    setPositionAttribute(gl, buffers, programInfo);
+    setTextureAttribute(gl, buffers, programInfo);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+    setNormalAttribute(gl, buffers, programInfo);
+    gl.useProgram(programInfo.program);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.normalMatrix, false, normalMatrix);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
+    const vertexCount = 36;
+    const type = gl.UNSIGNED_SHORT;
+    const offset = 0;
+    const realinitialmatrix = mat4.clone(modelViewMatrix);
+    var initialMatrix = mat4.clone(modelViewMatrix);
+    loadTexture(gl, new Uint8Array([200, 200, 200, 255]));
+    mat4.translate(modelViewMatrix, modelViewMatrix, [0, -50, 0]);
+    mat4.scale(modelViewMatrix, modelViewMatrix, [10000, 5, 10000]);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+    mat4.copy(modelViewMatrix, initialMatrix);
+    loadTexture(gl, new Uint8Array([50, 50, 50, 255]));
+    mat4.translate(modelViewMatrix, modelViewMatrix, [0, -15, 0]);
+    mat4.scale(modelViewMatrix, modelViewMatrix, [50, 30, 50]);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+    for (var i = -1; i < 2; i += 2) {
+        mat4.copy(modelViewMatrix, initialMatrix);
+        mat4.translate(modelViewMatrix, modelViewMatrix, [0, -40, 0]);
+        mat4.rotate(modelViewMatrix, modelViewMatrix, Math.PI / 4, [0, i, 0]);
+        mat4.scale(modelViewMatrix, modelViewMatrix, [350, 5, 15]);
+        gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+        mat4.copy(modelViewMatrix, initialMatrix);
+    }
+    mat4.rotate(modelViewMatrix, modelViewMatrix, Math.PI * stepperpos / 100, [0, -1, 0]);
+    for (let i = 0; i < 4; i++) {
+        const initialMatrix = mat4.clone(modelViewMatrix);
+        mat4.translate(modelViewMatrix, modelViewMatrix, [positions[i][0] * 2, positions[i][1] * 2, 0]);
+        mat4.rotate(modelViewMatrix, modelViewMatrix, positions[i][2], [0, 0, 1]);
+        loadTexture(gl, new Uint8Array([50, 50, 50, 255]));
+        mat4.scale(modelViewMatrix, modelViewMatrix, [20, positions[i][3], 20]);
+        gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+        mat4.copy(modelViewMatrix, initialMatrix);
+    }
+    loadTexture(gl, new Uint8Array([250, 250, 250, 255]));
+    for (let i = -1; i <= 1; i += 2) {
+        const initialMatrix = mat4.clone(modelViewMatrix);
+        mat4.translate(modelViewMatrix, modelViewMatrix, [positions[4][0] * 2 + (15 * Math.cos(angle)), positions[4][1] * 2 - 20, i * (15 * Math.sin(angle))]);
+        mat4.rotate(modelViewMatrix, modelViewMatrix, Math.PI / 2, [0, 0, 1]);
+        mat4.rotate(modelViewMatrix, modelViewMatrix, i * -angle, [1, 0, 0]);
+        mat4.scale(modelViewMatrix, modelViewMatrix, [5, 15, 5]);
+        gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+        mat4.copy(modelViewMatrix, initialMatrix);
+        mat4.translate(modelViewMatrix, modelViewMatrix, [positions[4][0] * 2 + (30 * Math.cos(angle) + 15 * Math.cos(angle - Math.PI / 4)), positions[4][1] * 2 - 20, i * (30 * Math.sin(angle) - 15 * Math.cos(angle + Math.PI / 4))]);
+        mat4.rotate(modelViewMatrix, modelViewMatrix, Math.PI / 2, [0, 0, 1]);
+        mat4.rotate(modelViewMatrix, modelViewMatrix, i * -(angle - Math.PI / 4), [1, 0, 0]);
+        mat4.scale(modelViewMatrix, modelViewMatrix, [5, 15, 5]);
+        gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+        mat4.copy(modelViewMatrix, initialMatrix);
+    }
+    loadTexture(gl, new Uint8Array([0, 200, 50, 255]));
+    initialMatrix = mat4.clone(modelViewMatrix);
+    mat4.translate(modelViewMatrix, modelViewMatrix, [25, 50, 0]);
+    mat4.scale(modelViewMatrix, modelViewMatrix, [2, 25, 25]);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+    mat4.copy(modelViewMatrix, initialMatrix);
+    loadTexture(gl, new Uint8Array([50, 50, 50, 255]));
+    mat4.translate(modelViewMatrix, modelViewMatrix, [28, 50, 0]);
+    mat4.scale(modelViewMatrix, modelViewMatrix, [2, 15, 10]);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+    mat4.copy(modelViewMatrix, initialMatrix);
+    loadTexture(gl, new Uint8Array([255, 0, 0, 255]));
+    for (let i = 0; i < barrels.length; i++) {
+        if (barrels[i].attached == "yes") {
+            const initialMatrix = mat4.clone(modelViewMatrix);
+            mat4.translate(modelViewMatrix, modelViewMatrix, [positions[4][0] * 2 + 40, positions[4][1] * 2 - 15, 0]);
+            mat4.scale(modelViewMatrix, modelViewMatrix, [15, 30, 15]);
+            gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+            gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+            mat4.copy(modelViewMatrix, initialMatrix);
+        }
+    }
+    mat4.copy(modelViewMatrix, realinitialmatrix);
+    for (let i = 0; i < barrels.length; i++) {
+        if (barrels[i].attached != "yes") {
+            const initialMatrix = mat4.clone(modelViewMatrix);
+            mat4.translate(modelViewMatrix, modelViewMatrix, [barrels[i].position[0] * 2, -15, barrels[i].position[1] * 2]);
+            mat4.scale(modelViewMatrix, modelViewMatrix, [15, 30, 15]);
+            gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+            gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+            mat4.copy(modelViewMatrix, initialMatrix);
+        }
+    }
+}
+function drawSceneForPicking(gl, programInfo, buffers, cameraRotationX, cameraRotationY, zoom, barrels) {
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    const fieldOfView = (45 * Math.PI) / 180;
+    const aspect = 640 / 480;
+    const zNear = 0.1;
+    const zFar = 1000.0;
+    const projectionMatrix = mat4.create();
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+    let modelViewMatrix = mat4.create();
+    const xpos = -zoom * Math.sin(cameraRotationX) * Math.cos(cameraRotationY);
+    const ypos = zoom * Math.sin(cameraRotationY);
+    const zpos = zoom * Math.cos(cameraRotationX) * Math.cos(cameraRotationY);
+    mat4.lookAt(modelViewMatrix, [xpos, ypos, zpos], [0, 0, 0], [0, 1, 0]);
+    gl.useProgram(programInfo.program);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
+    gl.disableVertexAttribArray(programInfo.attribLocations.vertexNormal);
+    gl.disableVertexAttribArray(programInfo.attribLocations.textureCoord);
+    setPositionAttribute(gl, buffers, programInfo);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+    const vertexCount = 36;
+    const type = gl.UNSIGNED_SHORT;
+    const offset = 0;
+    barrels.forEach((barrel, i) => {
+        if (barrel.attached)
+            loadTexture(gl, new Uint8Array(barrel.colorID));
+        const initialMatrix = mat4.clone(modelViewMatrix);
+        mat4.translate(modelViewMatrix, modelViewMatrix, [barrels[i].position[0] * 2, -15, barrels[i].position[1] * 2]);
+        mat4.scale(modelViewMatrix, modelViewMatrix, [15, 30, 15]);
+        gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+        mat4.copy(modelViewMatrix, initialMatrix);
+    });
+}
+function setPositionAttribute(gl, buffers, programInfo) {
+    const numComponents = 3;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, numComponents, type, normalize, stride, offset);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+}
+function setTextureAttribute(gl, buffers, programInfo) {
+    const num = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+    gl.vertexAttribPointer(programInfo.attribLocations.textureCoord, num, type, normalize, stride, offset);
+    gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
+}
+function setNormalAttribute(gl, buffers, programInfo) {
+    const numComponents = 3;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.normal);
+    gl.vertexAttribPointer(programInfo.attribLocations.vertexNormal, numComponents, type, normalize, stride, offset);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexNormal);
+}
+function loadTexture(gl, color) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    const level = 0;
+    const internalFormat = gl.RGBA;
+    const width = 1;
+    const height = 1;
+    const border = 0;
+    const srcFormat = gl.RGBA;
+    const srcType = gl.UNSIGNED_BYTE;
+    const pixel = new Uint8Array(color);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, srcFormat, srcType, pixel);
+    return texture;
+}
+export { drawScene, drawSceneForPicking };
---
+++ static/main.js
@@ -0,0 +1,185 @@
+import { initBuffers } from "./cube-buffer.js";
+import { drawScene, drawSceneForPicking } from "./draw-scene.js";
+var mousedown = false;
+var mousePos = { x: 0, y: 0 };
+var prevmouse = { x: 0, y: 0 };
+var positions = [[0, 29.75, 0.0, 59.5 + 4], [6.283683576271408, 92.71077115505295, 2.9545969675064323, 67.6 + 4], [43.78368357627141, 112.96077115505295, 1.1772622130201693, 67.6 + 4], [87.5, 100.0, 1.5707963267948966, 25], [100, 100]];
+var angle = Math.PI / 4;
+var stepperpos = 100;
+var barrels = [];
+var websocket = new WebSocket("ws://192.168.137.81:8765");
+function updateInfo() {
+    const x = document.getElementById("xpos").value;
+    const y = document.getElementById("ypos").value;
+    const step = document.getElementById("step").value;
+    websocket.send(`${x} ${y} ${step}`);
+}
+function takePhoto() {
+    websocket.send("photo");
+}
+function scan() {
+    websocket.send("scan");
+}
+function moveClaw(checkbox) {
+    if (checkbox.checked) {
+        angle = Math.PI / 2;
+        websocket.send("claw 45");
+    }
+    else {
+        websocket.send("claw 0");
+        angle = Math.PI / 4;
+    }
+}
+websocket.onmessage = (event) => {
+    if (event.data == "image") {
+        const img = document.getElementById('camera');
+        img.src = `static/image.jpg?${Math.ceil(Math.random() * 1000)}`;
+    }
+    else if (event.data.startsWith("barrel ")) {
+        var info = event.data.split(" ");
+        barrels.push({ position: [parseFloat(info[1]), parseFloat(info[2])], colorID: [255, barrels.length * 10, 0, 255, 0], attached: "no" });
+    }
+    else if (event.data.startsWith("stepperpos")) {
+        stepperpos = parseInt(event.data.split(" ")[1]);
+        console.log(stepperpos);
+    }
+    else if (event.data.startsWith("claw ")) {
+        angle = parseFloat(event.data.split(" ")[1]);
+    }
+    else if (event.data == "attached") {
+        for (var i = 0; i < barrels.length; i++) {
+            if (barrels[i].attached == "next") {
+                barrels[i].attached = "yes";
+            }
+        }
+    }
+    else if (event.data.startsWith("dropped")) {
+        for (var i = 0; i < barrels.length; i++) {
+            if (barrels[i].attached == "yes") {
+                barrels[i].attached = "no";
+                barrels[i].position = event.data.split(" ").slice(1);
+            }
+        }
+    }
+    else {
+        var obj = JSON.parse(event.data);
+        positions = obj;
+    }
+};
+window.updateInfo = updateInfo;
+window.moveClaw = moveClaw;
+window.takePhoto = takePhoto;
+window.scan = scan;
+main();
+function main() {
+    const canvas = document.querySelector("#glcanvas");
+    const gl = canvas.getContext("webgl");
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    const vsSource = `
+        attribute vec4 aVertexPosition;
+        attribute vec3 aVertexNormal;
+        attribute vec2 aTextureCoord;
+        uniform mat4 uNormalMatrix;
+        uniform mat4 uModelViewMatrix;
+        uniform mat4 uProjectionMatrix;
+        varying highp vec2 vTextureCoord;
+        varying highp vec3 vLighting;
+        void main(void) {
+            gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+            vTextureCoord = aTextureCoord;
+            highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
+            highp vec3 directionalLightColor = vec3(1, 1, 1);
+            highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
+            highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+            highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+            vLighting = ambientLight + (directionalLightColor * directional);
+        }
+    `;
+    const fsSource = `
+        varying highp vec2 vTextureCoord;
+        varying highp vec3 vLighting;
+        uniform sampler2D uSampler;
+        void main(void) {
+        highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
+        gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
+        }
+    `;
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+            vertexNormal: gl.getAttribLocation(shaderProgram, "aVertexNormal"),
+            textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
+            modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+            normalMatrix: gl.getUniformLocation(shaderProgram, "uNormalMatrix"),
+            uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
+        },
+    };
+    const buffers = initBuffers(gl);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    function render() {
+        drawScene(gl, programInfo, buffers, mousePos.x, mousePos.y, positions, 1000, angle, barrels, stepperpos);
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+    canvas.addEventListener('mousemove', function (e) {
+        getMousePosition(e);
+    });
+    addEventListener("click", function (e) {
+        const target = e.currentTarget;
+        const rect = target.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        drawSceneForPicking(gl, programInfo, buffers, mousePos.x, mousePos.y, 1000, barrels);
+        var pixels = new Uint8Array(4);
+        gl.readPixels(x, rect.height - y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+        const scale = 255 / Math.max(...pixels.subarray(0, 3));
+        for (let i = 0; i < pixels.length; i++) {
+            pixels[i] = pixels[i] * scale;
+        }
+        console.log(pixels);
+        if (pixels[0] == 255 && pixels[2] == 0) {
+            websocket.send("barrel " + barrels[Math.round(pixels[1] / 10)].position[0] + " " + barrels[Math.round(pixels[1] / 10)].position[0]);
+            console.log("barrel");
+        }
+    });
+}
+function initShaderProgram(gl, vsSource, fsSource) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    return shaderProgram;
+}
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    return shader;
+}
+addEventListener("mousedown", (event) => {
+    mousedown = true;
+    const rect = event.target.getBoundingClientRect();
+    prevmouse.x = ((event.clientX - rect.left) / rect.width * 2 - 1) * 4;
+    prevmouse.y = ((event.clientY - rect.top) / rect.height * 2 - 1) * 2;
+});
+addEventListener("mouseup", (_) => {
+    mousedown = false;
+});
+function getMousePosition(event) {
+    const target = event.currentTarget;
+    if (mousedown) {
+        const rect = target.getBoundingClientRect();
+        const x = ((event.clientX - rect.left) / rect.width * 2 - 1) * 4;
+        const y = ((event.clientY - rect.top) / rect.height * 2 - 1) * 2;
+        mousePos = { x: mousePos.x + x - prevmouse.x, y: mousePos.y + y - prevmouse.y };
+        prevmouse = { x: x, y: y };
+    }
+}
