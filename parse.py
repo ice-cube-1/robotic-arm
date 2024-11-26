6070ca6aea5d1547cafabe7519c5986731a40abd
ice-cube-1 <alice.goward@spgs.org>
2024-11-26 20:39:08 +0000

    no change

--- parse.py
+++ parse.py
@@ -1,9 +1,16 @@
-from movement import scan
+from movement import scan, pickup, drop, move, rotate
 
-def parse():#arm, camera, websocket, barrels):
+def parse(arm, camera, websocket, barrels):
     code = "".join(open("sampleCode.txt").readlines())
+    old = ["scan(", "pickup(", "drop(", "rotate(", "move("]
+    new = ["scan(arm, camera, websocket, barrels, ", 
+           "pickup(arm, websocket, barrels, ", 
+           "drop(arm, websocket, barrels, ",
+           "rotate(arm, websocket, ",
+           "move(arm, websocket, "]
     i=0
     while i < len(code):
+        addParams(code,)
         if len(code)-i > len("scan()") and code[i:i+len("scan()")] == "scan()":
             code = code[:i]+"scan(arm, camera, websocket, barrels)"+code[i+len("scan()"):]
         if len(code)-i > len("pickup(") and code[i:i+len("pickup(")] == "pickup(":
@@ -13,4 +20,8 @@ def parse():#arm, camera, websocket, barrels):
         i+=1
     print(code)
     exec(code)
-parse()
+
+def addParams(code, old: list[str], new: list[str], i):
+    for j in range(len(old)):
+        if len(code)-i > len(old[j]) and code[i:i+len(old[j])] == old[j]:
+            code = code[:i]+new[j]+code[i+len(old[j]):]
