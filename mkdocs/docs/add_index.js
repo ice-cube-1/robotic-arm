7e27e6769390a0090c1ab3112b2d4ccfaf511532
ice-cube-1 <alice.goward@spgs.org>
2025-01-06 17:10:54 +0000

    fixed github link on docs

--- mkdocs/docs/add_index.js
+++ mkdocs/docs/add_index.js
@@ -1,4 +1,6 @@
 var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
-    links[i].href = links[i].href+"index.html"
+    if (!links[i].href.startsWith("https")) {
+        links[i].href = links[i].href+"index.html"
+    }
 }
